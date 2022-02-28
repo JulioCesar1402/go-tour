3c6d57f7171c08a9bb31bc0930584d6fb49ab5c0
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2022-02-28 17:07:47 -0300

    feat: rewrite func names

---  Chapter_2/ifs.go
+++  Chapter_2/ifs.go
@@ -2,14 +2,14 @@ package main
 
 import "fmt"
 
-func simple_if(x int) string {
+func simpleIf(x int) string {
 	if x < 10 {
 		return "Yeahh, it's greater than 10"
 	}
 	return "Sorry, it's lower than 10"
 }
 
-func if_with_short_declaration(x int) string {
+func ifWithShortDeclaration(x int) string {
 	if x < 10 {
 		return "Yeahh, it's greater than 10"
 	}
@@ -17,8 +17,8 @@ func if_with_short_declaration(x int) string {
 }
 
 func main() {
-	t := simple_if(1)
-	q := simple_if(23)
+	t := simpleIf(1)
+	q := simpleIf(23)
 	fmt.Println(t)
 	fmt.Println(q)
 }
