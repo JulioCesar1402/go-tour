3614f378be53c624ad080e78d7d9cdf79f7c636d
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2022-02-28 17:12:34 -0300

    feat: ifs

---  Chapter_2/ifs.go
+++  Chapter_2/ifs.go
@@ -2,23 +2,31 @@ package main
 
 import "fmt"
 
-func simpleIf(x int) string {
+// simple if
+func isGreaterThenTen(x int) string {
 	if x < 10 {
 		return "Yeahh, it's greater than 10"
 	}
 	return "Sorry, it's lower than 10"
 }
 
-func ifWithShortDeclaration(x int) string {
-	if x < 10 {
-		return "Yeahh, it's greater than 10"
+// if with short declaration
+func positiveSubstract(x, y  int) int {
+	if z := x - y; x > y {
+		return z
+	} else {
+		return y - x
 	}
-	return "Sorry, it's lower than 10"
 }
 
 func main() {
-	t := simpleIf(1)
-	q := simpleIf(23)
+	t := isGreaterThenTen(1)
+	q := isGreaterThenTen(23)
 	fmt.Println(t)
 	fmt.Println(q)
+
+	a := positiveSubstract(2,3)
+	b := positiveSubstract(45,9)
+	fmt.Println(a)
+	fmt.Println(b)
 }
