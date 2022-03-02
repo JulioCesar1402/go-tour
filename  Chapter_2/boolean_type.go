72f000c9db74da66336a29e38d3c093adaa8392a
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2022-03-02 09:50:09 -0300

    style: lint errors

---  Chapter_2/boolean_type.go
+++  Chapter_2/boolean_type.go
@@ -4,13 +4,13 @@ import "fmt"
 
 var x bool
 
-func main()  {
+func main() {
 	fmt.Println(x) // zero value
 	x = (10 > 3)
 	fmt.Println(x) // true
 	y := (23 < 7)
 	z := (6 == 6)
-	j := ( 2 != 3)
+	j := (2 != 3)
 	fmt.Println(y) // false
 	fmt.Println(z) // true
 	fmt.Println(j) // true
