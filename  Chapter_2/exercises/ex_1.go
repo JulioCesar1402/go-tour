dc71db17baeed5bcfba14e6abcb93bffc2389025
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2022-03-13 10:33:35 -0300

    feat: exercises

---
+++  Chapter_2/exercises/ex_1.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 200
+	fmt.Printf("%d - %b - %#x\n", a, a, a)
+}
---
+++  Chapter_2/exercises/ex_2.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := (200 == 5)
+	b := (2 <= 5)
+	c := (65 > 9)
+	d:= (27 < 3)
+	e := (0 > -1)
+	f := (23 != 6)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+}
